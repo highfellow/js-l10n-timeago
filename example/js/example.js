1c70c8395d029ba7520236823444028b75c672d5
Andy Baxter <highfellow@gmail.com>
2012-12-14 08:01:20 +0000

    example times all roll over

--- example/js/example.js
+++ example/js/example.js
@@ -21,7 +21,7 @@ requirejs(["timeago", "l10n", "l10n-browser"], function(timeago, l10n, l10nBrows
       localeDiv.append("<h2>" + l('langHeader') + "</h2>");
       localeDiv.append("<p>" + l('rolloverMessage') + "</p>");
       var table = $("<table>").appendTo(localeDiv);
-      table.append("<thead><tr><th>" + l('unit') + "</th><th>" + l('unitMinutes') + "</th><th>" + l('now') + "</th><th>+ 1</th><th>+ 2</th><th>" + l('next') + " -1.5</th><th>" + l('next') + " -0.5</th></tr><thead>");
+      table.append("<thead><tr><th>" + l('unit') + "</th><th>" + l('unitMinutes') + "</th><th>" + l('now') + "</th><th>+ 1</th><th>+ 1.5</th><th>" + l('next') + " -1.5</th><th>" + l('next') + " -0.5</th></tr><thead>");
       var tbody = $("<tbody>").appendTo(table);
       var l10nUnits = $.fn.timeago.defaults.l10nUnits;
       var unit = 0;
@@ -30,8 +30,10 @@ requirejs(["timeago", "l10n", "l10n-browser"], function(timeago, l10n, l10nBrows
         var unitMillis = l10nUnit.minutes * 60000;
         var rowStr = '<tr><td>' + l('unit' + l10nUnit.l10nUnit) + '</td><td>' + l10nUnit.minutes + '</td><td>';
         if (unit === 0) rowStr += makeTimeElt(0);
-        rowStr += '</td><td>' + makeTimeElt(l10nUnit.minutes) + '</td>';
-        rowStr += '<td>' + makeTimeElt(l10nUnit.minutes * 2) + '</td>';
+        var mins = (unit === 0) ? 0 : l10nUnit.minutes - (l10nUnits[unit - 1].baseMinutes * 0.5) - 1;
+        rowStr += '</td><td>' + makeTimeElt(mins) + '</td>';
+        mins = (unit !== l10nUnits.length - 1) ? (l10nUnit.baseMinutes * 1.5) - 1: (l10nUnit.minutes * 1.5) - 1;
+        rowStr += '<td>' + makeTimeElt(mins) + '</td>';
         if (unit < l10nUnits.length - 1) {
           rowStr += '<td>' + makeTimeElt((l10nUnits[unit].base[0] - 1.5) * (l10nUnits[unit + 1].minutes / l10nUnits[unit].base[0]) - 1) + '</td>';
           rowStr += '<td>' + makeTimeElt((l10nUnits[unit].base[0] - 0.5) * (l10nUnits[unit + 1].minutes / l10nUnits[unit].base[0]) - 1) + '</td>';
